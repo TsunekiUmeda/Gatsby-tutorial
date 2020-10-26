c97e9c561bd31beeb8e23e884a5eb0c98902dba6
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-26 15:17:06 +0900

    Add Layout component

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const blog = () => {
+  return (
+    <Layout>
+      <h1>This is our blog Pages</h1>
+    </Layout>
+  )
+}
+
+export default blog
