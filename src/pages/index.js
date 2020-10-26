282d75fe318d5fbb556bc758ad6c151adf5f4a60
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-26 16:55:57 +0900

    add css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,9 @@ import React from "react"
 import Layout from "../components/Layout"
 
 export default function Home() {
-  return <Layout>Hello from Gatsby!</Layout>
+  return (
+    <Layout>
+      <h1>Hello from Gatsby!</h1>
+    </Layout>
+  )
 }
