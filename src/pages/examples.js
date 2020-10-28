cb9b1e30ff29b1029d9b2a018b0f4a8f572f8185
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-28 18:20:20 +0900

    StaticQuery example

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../examples/Header"
+import HeaderStatic from "../examples/HeaderStatic"
 import Layout from "../components/Layout"
 const examples = () => {
   return (
     <Layout>
       <h1>Hello from example page</h1>
       <Header />
+      <HeaderStatic />
     </Layout>
   )
 }
