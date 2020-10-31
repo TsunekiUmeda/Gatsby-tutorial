fc44799156e458f751cb1c786b117f40addf06b0
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-31 19:09:10 +0900

    install gatsby-image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,11 +14,20 @@ module.exports = {
   },
 
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
       },
     },
   ],
